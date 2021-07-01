0c8d635c058dfae9c4cd05b91c340fee17f4aba8
hpehl <harald.pehl@gmail.com>
2021-07-01 15:15:32 +0200

    Upgrade dependencies

---
+++ src/main/java/org/wildfly/modelgraph/MgtObjectMapper.java
@@ -0,0 +1,15 @@
+package org.wildfly.modelgraph;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.jackson.ObjectMapperCustomizer;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class MgtObjectMapper implements ObjectMapperCustomizer {
+
+    public void customize(ObjectMapper mapper) {
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+}
