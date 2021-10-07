ae3c83f6e766b6f417802f23ee65800291cc1a69
hpehl <harald.pehl@gmail.com>
2021-10-07 19:03:06 +0200

    Tweak Jackson serialization

--- src/main/java/org/wildfly/modelgraph/MgtObjectMapper.java
+++ src/main/java/org/wildfly/modelgraph/MgtObjectMapper.java
@@ -1,15 +1,20 @@
 package org.wildfly.modelgraph;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.quarkus.jackson.ObjectMapperCustomizer;
-
 import javax.inject.Singleton;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.quarkus.jackson.ObjectMapperCustomizer;
 
 @Singleton
 public class MgtObjectMapper implements ObjectMapperCustomizer {
 
     public void customize(ObjectMapper mapper) {
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.setSerializationInclusion(Include.NON_NULL);
+        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.PUBLIC_ONLY);
     }
 }
